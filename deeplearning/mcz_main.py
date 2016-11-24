e7bea4414ef4043858000a36aee313d4864b7cd4
toru takahashi <takahashino3@gmail.com>
2016-11-24 15:10:30 +0900

    一回し完了

--- deeplearning/mcz_main.py
+++ deeplearning/mcz_main.py
@@ -76,7 +76,7 @@ def main(ckpt = None):
                 summary_str = sess.run(summary_op,feed_dict={keep_prob: 1.0})
                 summary_writer.add_summary(summary_str, step)
 
-            if step % 1000 == 0 or (step + 1) == FLAGS.max_steps or loss_result == 0:
+            if step % 500 == 0 or (step + 1) == FLAGS.max_steps or loss_result == 0:
                 checkpoint_path = os.path.join(FLAGS.train_dir, 'model.ckpt')
                 saver.save(sess, checkpoint_path, global_step=step)
 
